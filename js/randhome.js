d270a474f59f91a81589fea0ed8a8f4381e341ae
pedroppb <55588996+pedroppb@users.noreply.github.com>
2020-08-28 10:10:20 -0300

    Update randhome.js

--- js/randhome.js
+++ js/randhome.js
@@ -1,5 +1,5 @@
 function randhome (min, max) {
   var article = document.getElementById("article");
   rand=Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
-  article.style.backgroundImage= "url('../img/"+rand+".jpg')";
+  article.style.backgroundImage= "url('img/bg"+rand+".jpg')";
 }
